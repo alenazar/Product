1f8b90490d92778094c3ea787e164d90498124f2
user <alena_zar@mail.ru>
2022-10-05 13:19:52 +0300

    final of test

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -3,19 +3,51 @@ import org.junit.jupiter.api.Test;
 
 public class ProductManagerTest {
     Book testBook = new Book(100,"Муму",20,"Тургенев");
-    Book testBook2 = new Book(110,"Война и Мир",25,"Толстой");
+    Book testBook2 = new Book(110,"Война и Мур",25,"Толстой");
     Smartphone testSmartphone = new Smartphone(101,"Iphone 6",700,"Apple");
 
     @Test
-    public void searchBy() {
+    public void searchByOneFound() {
         ProductManager productManager = new ProductManager();
         productManager.add(testBook);
+        productManager.add(testBook2);
         productManager.add(testSmartphone);
 
-        int expected = 1;
-        int actual = productManager.searchBy("Му").length;
+        Product[] expected = new Product[1];
+        expected[0] = testBook;
+        Product[] actual = productManager.searchBy("му");
 
-        Assertions.assertEquals(expected, actual);
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void searchByTwoFound() {
+        ProductManager productManager = new ProductManager();
+        productManager.add(testBook);
+        productManager.add(testBook2);
+        productManager.add(testSmartphone);
+
+        Product[] expected = new Product[2];
+        expected[0] = testBook;
+        expected[1] = testBook2;
+        Product[] actual = productManager.searchBy("Му");
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void searchByEmptyFound() {
+        ProductManager productManager = new ProductManager();
+        productManager.add(testBook);
+        productManager.add(testBook2);
+        productManager.add(testSmartphone);
+
+        Product[] expected = new Product[0];
+        Product[] actual = productManager.searchBy("па");
+
+        Assertions.assertArrayEquals(expected, actual);
 
     }
 
